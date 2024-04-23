d9621e522f186df49fe756c1c60f5064c9d07558
Jin Park <jinpark1504@gmail.com>
2024-04-23 16:12:58 +0900

    modifier

---
+++ UsingModifier/UsingModifier/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  UsingModifier
+//
+//  Created by Jungjin Park on 2024-04-23.
+//
+
+import SwiftUI
+
+struct BackgroundStyle: ViewModifier {
+    var bgColor: Color
+    
+    func body(content: Content) -> some View {
+        content
+            .frame(width: UIScreen.main.bounds.width * 0.3)
+            .foregroundStyle(.black)
+            .padding()
+            .background(bgColor)
+            .cornerRadius(20)
+    }
+}
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Perfect")
+                .modifier(BackgroundStyle(bgColor: .red))
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UsingModifier/UsingModifier/UsingModifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsingModifierApp.swift
+//  UsingModifier
+//
+//  Created by Jungjin Park on 2024-04-23.
+//
+
+import SwiftUI
+
+@main
+struct UsingModifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
