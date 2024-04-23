bf70e202579288913561258b0b30a62d9c126bd7
Jin Park <jinpark1504@gmail.com>
2024-04-23 17:19:55 +0900

    SFSymbols

---
+++ UsingSFSymbols/UsingSFSymbols/UsingSFSymbolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsingSFSymbolsApp.swift
+//  UsingSFSymbols
+//
+//  Created by Jungjin Park on 2024-04-23.
+//
+
+import SwiftUI
+
+@main
+struct UsingSFSymbolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
