cd4c7cf8439297a6caa348fbec1b8446737dfce4
Jin Park <jinpark1504@gmail.com>
2024-04-24 13:48:12 +0900

    List .searchable

---
+++ SearchList/SearchList/SearchListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchListApp.swift
+//  SearchList
+//
+//  Created by Jungjin Park on 2024-04-24.
+//
+
+import SwiftUI
+
+@main
+struct SearchListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
