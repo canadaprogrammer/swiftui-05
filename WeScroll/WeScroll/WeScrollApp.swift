c5e14ef8298ca023b980d36b599cc04f266912a1
Jin Park <jinpark1504@gmail.com>
2024-04-24 10:15:15 +0900

    ScrollView and ScrollIndicatorsFlash

---
+++ WeScroll/WeScroll/WeScrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeScrollApp.swift
+//  WeScroll
+//
+//  Created by Jungjin Park on 2024-04-24.
+//
+
+import SwiftUI
+
+@main
+struct WeScrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
