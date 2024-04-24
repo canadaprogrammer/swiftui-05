e4e7754b3ee1f6dbe5ffa4c2c2075b815486f512
Jin Park <jinpark1504@gmail.com>
2024-04-24 11:50:02 +0900

    editable list

---
+++ EditableList/EditableList/EditableListApp.swift
@@ -0,0 +1,17 @@
+//
+//  EditableListApp.swift
+//  EditableList
+//
+//  Created by Jungjin Park on 2024-04-24.
+//
+
+import SwiftUI
+
+@main
+struct EditableListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
