93ce233ab326d44ac55b1f842ca0935802515ca0
Jin Park <jinpark1504@gmail.com>
2024-04-24 10:42:56 +0900

    StaticList

---
+++ StructList/StructList/StructListApp.swift
@@ -0,0 +1,17 @@
+//
+//  StructListApp.swift
+//  StructList
+//
+//  Created by Jungjin Park on 2024-04-24.
+//
+
+import SwiftUI
+
+@main
+struct StructListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
