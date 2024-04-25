b3d1ff31df44c124865b6f8ed4eafbab6c76b118
Jin Park <jinpark1504@gmail.com>
2024-04-25 09:37:47 +0900

    LazyVGrid and LazyHGrid

---
+++ LazyGrids/LazyGrids/LazyGridsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyGridsApp.swift
+//  LazyGrids
+//
+//  Created by Jungjin Park on 2024-04-25.
+//
+
+import SwiftUI
+
+@main
+struct LazyGridsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
