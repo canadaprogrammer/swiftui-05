f537d99c795247599483ff06e75e5a9d657c6c8e
Jin Park <jinpark1504@gmail.com>
2024-04-25 11:01:37 +0900

    ScrollPosition (iOS17+) and ScrollViewReader(iOS14+)

---
+++ ScrollViewReaders/ScrollViewReaders/ScrollViewReadersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollViewReadersApp.swift
+//  ScrollViewReaders
+//
+//  Created by Jungjin Park on 2024-04-25.
+//
+
+import SwiftUI
+
+@main
+struct ScrollViewReadersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
