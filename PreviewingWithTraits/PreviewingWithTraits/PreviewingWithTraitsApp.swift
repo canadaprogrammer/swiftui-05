1014a797c319516b858aa80704a45d773a492d50
Jin Park <jinpark1504@gmail.com>
2024-04-26 16:22:43 +0900

    Preview with Traits

---
+++ PreviewingWithTraits/PreviewingWithTraits/PreviewingWithTraitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PreviewingWithTraitsApp.swift
+//  PreviewingWithTraits
+//
+//  Created by Jungjin Park on 2024-04-26.
+//
+
+import SwiftUI
+
+@main
+struct PreviewingWithTraitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
