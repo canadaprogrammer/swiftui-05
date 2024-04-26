2dd05a48c1d80839ea513f787dbaf934fefceb55
Jin Park <jinpark1504@gmail.com>
2024-04-26 16:40:33 +0900

    sign up

---
+++ SignUp/SignUp/SignUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignUpApp.swift
+//  SignUp
+//
+//  Created by Jungjin Park on 2024-04-26.
+//
+
+import SwiftUI
+
+@main
+struct SignUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
