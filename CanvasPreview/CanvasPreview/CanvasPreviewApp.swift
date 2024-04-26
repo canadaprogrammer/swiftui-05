b73be1b580072bba8ed5377ec5d6227f548853a1
Jin Park <jinpark1504@gmail.com>
2024-04-26 11:18:17 +0900

    Canvas Preview

---
+++ CanvasPreview/CanvasPreview/CanvasPreviewApp.swift
@@ -0,0 +1,17 @@
+//
+//  CanvasPreviewApp.swift
+//  CanvasPreview
+//
+//  Created by Jungjin Park on 2024-04-26.
+//
+
+import SwiftUI
+
+@main
+struct CanvasPreviewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
